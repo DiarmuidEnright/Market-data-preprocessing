0fcce0bc25fe948775a622d7c21935321e5c0e1e
Diarmuid Enright <diarmuidenright29@gmail.com>
2025-04-06 17:20:02 +0100

    Still cleaning up the comments from the processing files

--- src/marketdata/MarketDataTransformer.java
+++ src/marketdata/MarketDataTransformer.java
@@ -20,15 +20,20 @@ public class MarketDataTransformer {
             List<MarketData> list = entry.getValue();
             Collections.sort(list, Comparator.comparingLong(MarketData::getTimestamp));
             List<Double> averages = new ArrayList<>();
+            //finding the moving average of all of the incoming data
             for (int i = 0; i <= list.size() - window; i++) {
                 double sum = 0;
                 for (int j = i; j < i + window; j++) {
+                    //adding the price to the sum
                     sum += list.get(j).getPrice();
                 }
+                //adding the average (total sum/ amount of value in the window eg: window) to our averages ArrayList
                 averages.add(sum / window);
             }
+            //adding to the HashMap with the key being the default entry for the map and the values being the found averages
             movingAverages.put(entry.getKey(), averages);
         }
+        //returning the HashMap
         return movingAverages;
     }
     public Map<String, List<MarketData>> resampleByInterval(List<MarketData> data, long interval) {
@@ -46,9 +51,11 @@ public class MarketDataTransformer {
             int count = 0;
             for (MarketData md : list) {
                 if (md.getTimestamp() - currentIntervalStart < interval) {
+                    //comparing the interval for the pricing window
                     sum += md.getPrice();
                     count++;
                 } else {
+                    //adding the sample for count 1 of the price at some timestamp for the symbol
                     sampled.add(new MarketData(md.getSymbol(), sum / count, currentIntervalStart));
                     currentIntervalStart = md.getTimestamp();
                     sum = md.getPrice();
@@ -56,8 +63,10 @@ public class MarketDataTransformer {
                 }
             }
             if (count > 0) sampled.add(new MarketData(entry.getKey(), sum / count, currentIntervalStart));
+            //adding the resampled data to our resamples HashMap using the default entry keys
             resampled.put(entry.getKey(), sampled);
         }
+        //returning
         return resampled;
     }
 }
